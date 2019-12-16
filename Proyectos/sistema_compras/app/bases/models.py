222f43571cd2b77d84a97f4a8ef7c9cdffdc511f
rvndrupal <rodrigodrupal1@gmail.com>
2019-12-16 11:40:25 -0600

    Cambios para el modulo de  Facturación

--- Proyectos/sistema_compras/app/bases/models.py
+++ Proyectos/sistema_compras/app/bases/models.py
@@ -14,3 +14,14 @@ class ClaseModelo(models.Model):
         abstract=True  #muy importante no hace la migración ya que solo lo vamos a tomar para heredar.
 
 
+# class ClaseModelo2(models.Model):
+#     estado = models.BooleanField(default=True)
+#     fc = models.DateTimeField(auto_now_add=True)
+#     fm = models.DateTimeField(auto_now=True)
+#     # uc = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # um = models.IntegerField(blank=True,null=True)
+#     uc = UserForeignKey(auto_user_add=True,related_name='+')
+#     um = UserForeignKey(auto_user=True,related_name='+')
+
+#     class Meta:
+#         abstract=True
