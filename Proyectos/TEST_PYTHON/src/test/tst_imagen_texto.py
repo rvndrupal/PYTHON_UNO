4059a054cb7eb43be8f232b883a7a572df07d66c
rvndrupal <rodrigodrupal1@gmail.com>
2019-12-20 16:23:55 -0600

    Cambios de Test

--- Proyectos/TEST_PYTHON/src/test/tst_imagen_texto.py
+++ Proyectos/TEST_PYTHON/src/test/tst_imagen_texto.py
@@ -4,7 +4,7 @@ import pytesseract
 
 
 
-imagen=cv2.imread("carloss.jpg") #lee la imagen
+imagen=cv2.imread("img5.jpg") #lee la imagen
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
 texto=pytesseract.image_to_string(imagen)
 print(texto)
